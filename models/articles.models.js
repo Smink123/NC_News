e6417dc52151424e5215bd027da20824b62f0ca2
Sarah Harvey <sarahcharvey@hotmail.com>
2024-01-16 11:30:56 +0000

    completed section 6

--- models/articles.models.js
+++ models/articles.models.js
@@ -89,4 +89,18 @@ exports.fetchAllArticles = () => {
                         return articles.rows;
                      });
                   });
-               };
+};
+
+exports.fetchCommentByArticleId = (article_id) => {
+  return db.query(`SELECT * FROM comments 
+  WHERE article_id = $1
+  ORDER BY created_at DESC`, [article_id]).then((comments) => {
+    if (comments.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "ID not found"
+      })
+    }
+    return comments.rows
+  })
+}
