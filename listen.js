f012d7515b9e5dd6d48d16458748ffc847534485
Sarah Harvey <sarahcharvey@hotmail.com>
2024-01-17 21:22:48 +0000

    made changed to production variables

--- listen.js
+++ listen.js
@@ -1,9 +1,15 @@
-const app = require('./app')
+// const app = require('./app')
 
-app.listen(8080, (err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("listening on 8080!")
-    }
-})
+// app.listen(8080, (err) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("listening on 8080!")
+//     }
+// })
+
+
+const app = require("./app");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
